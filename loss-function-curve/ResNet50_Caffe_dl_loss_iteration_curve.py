d0519422b1fe6cfb2dcd6965ec77caf5108a5471
root <yywu@szucla.org>
2017-10-31 11:17:40 +0800

    去掉最初的50次迭代,zoom in看一下之后迭代的loss走势,现在的Loss function曲线被初始的较大loss遮盖掉了后续loss的走势.

--- loss-function-curve/ResNet50_Caffe_dl_loss_iteration_curve.py
+++ loss-function-curve/ResNet50_Caffe_dl_loss_iteration_curve.py
@@ -25,21 +25,23 @@ test_net_loss = df['test_net_loss'].astype(float)
 train_net_loss = df['train_net_loss'].astype(float)
 
 x = range(len(loss_time))
-# 去掉最初的50次迭代,zoom in看一下之后迭代的loss走势,现在的Loss function曲线被初始的较大loss遮盖掉了后续loss的走势.
-new_x = x[50:]
 y_1 = test_net_loss
 y_2 = train_net_loss
+# 去掉最初的50次迭代,zoom in看一下之后迭代的loss走势,现在的Loss function曲线被初始的较大loss遮盖掉了后续loss的走势.
+new_x = x[50:]
+new_y_1 = y_1[50:]
+new_y_2 = y_2[50:]
 
 # plot
 # Create two subplots sharing y axis
 # fig, (ax1, ax2) = plt.subplots(2, sharey=True)
 fig, (ax2) = plt.subplots(1, sharey=True)
 
-# ax1.plot(new_x, y_1, 'C1')
+# ax1.plot(new_x, new_y_1, 'C1')
 # ax1.set(title='Lung nodule\'s Caffe deep learning loss', ylabel='Test net loss')
 # ax1.set(title='ResNet50 Caffe deep learning loss', ylabel='Test net loss')
 
-ax2.plot(new_x, y_2, 'C2')
+ax2.plot(new_x, new_y_2, 'C2')
 ax2.set(title='ResNet50 Caffe deep learning loss', xlabel='Iteration', ylabel='Train net loss')
 
 plt.show()
